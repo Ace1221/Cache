22d63c1dba1712cb834c7a3e906919fe3fffbc05
Ace1221 <herothemaster123@gmail.com>
2021-05-31 19:31:43 +0200

    Update project.pl
    First Predicate

--- project.pl
+++ project.pl
@@ -1,1 +1,20 @@
-test(e).
+:- use_module(library(clpfd)).
+sum_list(L,X):-
+                  sum_list(L,0,X).
+sum_list([],X,X).
+sum_list([H|T], A, S) :-
+                             A2 is A + H, 
+							 sum_list(T,A2,S).
+convertBinToDec(Bin,Dec):-
+                             convertBinToDec(Bin,0,[],Dec).
+							 
+convertBinToDec(0,_,List,Dec):- 
+                                     sum_list(List,Dec).
+convertBinToDec(Bin,Pos,List,Dec):-
+                                     Bin \= 0,
+                                     Num is (Bin mod 10),
+									 Rest is (Bin // 10),
+									 In is ((Num) * (2**Pos)),
+									 append(List,[In],List1),
+									 Pos1 is Pos+1,
+									 convertBinToDec(Rest,Pos1,List1,Dec).
