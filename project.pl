00a3d4c927a6243886e3f5da93a5c2c6cfb87690
Ace1221 <herothemaster123@gmail.com>
2021-06-06 18:34:50 +0200

    Main project so far

--- project.pl
+++ project.pl
@@ -1,4 +1,4 @@
-%--------------------------------------------------------------------------------------------------------------------------------------------------------%
+%------------------------------------------------------------------------  General  -------------------------------------------------------------------------%
 convertBinToDec(Bin,Dec):-
                              convertBinToDec(Bin,0,[],Dec).
 							 
@@ -50,15 +50,16 @@ replaceIthItem(Item,[H|T],I,[H|T1]):-
 logBase2(N,Res):-
                      log10(N,Res1),
 					 log10(2,Res2),
-					 Res is Res1 / Res2.
+					 ResC is Res1 / Res2,
+					 Res is ceiling(ResC).
 %--------------------------------------------------------------------------------------------------------------------------------------------------------%
 getNumBits(_,fullyAssoc,_,0).
 
 getNumBits(NumSets,setAssoc,_,NumBits):-
 											 logBase2(NumSets,NumBits).
 
-getNumBits(_,directMap,List,NumBits):-
-                                             length(List,N),
+getNumBits(_,directMap,Cache,NumBits):-
+                                             length(Cache,N),
 											 logBase2(N,NumBits).
 %--------------------------------------------------------------------------------------------------------------------------------------------------------%
 fillZeros(S,0,S).
@@ -69,22 +70,93 @@ fillZeros(S,N,R):-
                      fillZeros(S,N1,R1),
                      string_concat("0",R1,R).
 %--------------------------------------------------------------------------------------------------------------------------------------------------------%
-%Your code here%
-%--------------------------------------------------------------------------------------------------------------------------------------------------------%
-getData(StringAddress,OldCache,Mem,NewCache,Data,HopsNum,Type,BitsNum,hit):-
-                                                                                 getDataFromCache(StringAddress,OldCache,Data,HopsNum,Type,BitsNum),
-                                                                                 NewCache = OldCache.
 
-getData(StringAddress,OldCache,Mem,NewCache,Data,HopsNum,Type,BitsNum,miss):-
-                                                                                 \+getDataFromCache(StringAddress,OldCache,Data,HopsNum,Type,BitsNum),
-                                                                                 atom_number(StringAddress,Address),
-                                                                                 convertAddress(Address,BitsNum,Tag,Idx,Type),
-                                                                                 replaceInCache(Tag,Idx,Mem,OldCache,NewCache,Data,Type,BitsNum).
+
+%-----------------------------------------------------------------      Direct Map       ----------------------------------------------------------------%
+getDataFromCache(StringAddress
+,Cache,Data,HopsNum,directMap,BitsNum):-
+										     getNumBits(_,directMap,Cache,BitsNum),
+											 number_string(NumAddress,StringAddress),
+											 convertAddress(NumAddress,BitsNum,TagNum,IdxNum,directMap),
+											 convertBinToDec(IdxNum,Target),
+											 HopsNum is Target-1,
+											 nth0(Target,Cache,Result),
+											 Result = item(tag(Tag),data(Data),1,_),
+											 number_string(TagNum, Tag).
+											 
+%--------------------------------------------------------------------------------------------------------------------------------------------------------%
+convertAddress(Bin,BitsNum,Tag,Idx,directMap):-
+                                                 number_string(Bin,BinString),
+                                                 string_length(BinString,N),
+												 Len is 6 - N,
+												 fillZeros(BinString,Len,BinString1),
+                                                 string_concat(Tag1,Idx1,BinString1),
+												 string_length(Idx1,BitsNum),
+												 number_string(Tag,Tag1),
+												 number_string(Idx,Idx1).
+												 
+%--------------------------------------------------------------------------------------------------------------------------------------------------------%
+
+
+
+%-----------------------------------------------------------------   Fully Associative   ----------------------------------------------------------------%
+
+getDataFromCache(StringAddress, [item(tag(StringAddress),data(Data),HopsNum,_)|_], 
+Data, HopsNum, fullyAssoc, BitsNum).
+getDataFromCache(StringAddress,[item(tag(X),data(_),_,_)|T]
+,Data,HopsNum,fullyAssoc,BitsNum):-
+                                                             \+(X == StringAddress),
+                                                              getDataFromCache(StringAddress,T,Data,HopsNum,fullyAssoc,BitsNum).
+
+%--------------------------------------------------------------------------------------------------------------------------------------------------------%
+
+convertAddress(Bin,BitsNum,Tag,Idx,fullyAssoc):-
+                                                     atom_number(Tag1, Bin),
+                                                     atom_number(Tag1, Tag).
+													 
+%--------------------------------------------------------------------------------------------------------------------------------------------------------%
+
+
+%---------------------------------------------------------------- Replacing Blocks ----------------------------------------------------------------------%
+
+replaceInCache(Tag,Idx,Mem,OldCache
+,NewCache,ItemData,directMap,BitsNum):-
+                                         number_string(Tag,TagString),
+										 string_length(TagString,N),
+										 N1 is 6 - BitsNum,
+										 Ndiff is N1 - N,
+										 fillZeros(TagString,Ndiff,TagString1),
+										 number_string(Idx,IdxString),
+										 string_length(IdxString,Ni),
+										 NiDiff is BitsNum-Ni,
+										 fillZeros(IdxString,NiDiff,IdxString1),
+                                         string_concat(TagString1,IdxString1,StringMemAddress),
+										 number_string(NumMemAddress, StringMemAddress),
+										 convertBinToDec(NumMemAddress,NumMemIndex),
+										 nth0(NumMemIndex,Mem,ItemData),
+										 convertBinToDec(Idx,NumCacheIndex),
+										 replaceIthItem(item(tag(TagString1),data(ItemData),1,0),OldCache,NumCacheIndex,NewCache).
+%--------------------------------------------------------------------------------------------------------------------------------------------------------%
+
+
+%---------------------------------------------------------------- Main Predicates -----------------------------------------------------------------------%
+getData(StringAddress,OldCache,Mem,
+NewCache,Data,HopsNum,Type,BitsNum,hit):-
+                                             getDataFromCache(StringAddress,OldCache,Data,HopsNum,Type,BitsNum),
+                                             NewCache = OldCache.
+
+getData(StringAddress,OldCache,Mem,
+NewCache,Data,HopsNum,Type,BitsNum,miss):-
+                                             \+getDataFromCache(StringAddress,OldCache,Data,HopsNum,Type,BitsNum),
+                                             atom_number(StringAddress,Address),
+                                             convertAddress(Address,BitsNum,Tag,Idx,Type),
+                                             replaceInCache(Tag,Idx,Mem,OldCache,NewCache,Data,Type,BitsNum).
 %--------------------------------------------------------------------------------------------------------------------------------------------------------%
 runProgram([],OldCache,_,OldCache,[],[],Type,_).
+
 runProgram([Address|AdressList],OldCache,Mem,FinalCache,
 [Data|OutputDataList],[Status|StatusList],Type,NumOfSets):-
-                                                                 getNumBits(NumOfSets,Type,OldCache,BitsNum),
-                                                                 getData(Address,OldCache,Mem,NewCache,Data,HopsNum,Type,BitsNum,Status),
-                                                                 runProgram(AdressList,NewCache,Mem,FinalCache,OutputDataList,StatusList,
-                                                                 Type,NumOfSets).
+                                                             getNumBits(NumOfSets,Type,OldCache,BitsNum),
+                                                             getData(Address,OldCache,Mem,NewCache,Data,HopsNum,Type,BitsNum,Status),
+                                                             runProgram(AdressList,NewCache,Mem,FinalCache,OutputDataList,StatusList,
+                                                             Type,NumOfSets).
